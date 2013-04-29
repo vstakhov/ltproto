fecd44a11894bbce62ffa9eb1ae1329470e1f18e
Vsevolod Stakhov <vsevolod@highsecure.ru>
2013-04-29 13:53:57 +0100

    Fix test suite script.

--- test/run_test.sh
+++ test/run_test.sh
@@ -10,6 +10,7 @@ if [ F"$1" != F"-g" ] ; then
 
 	unlink $TEST_FILE 2> /dev/null
 	unlink $TEST_FILE.binded 2> /dev/null
+	unlink $TEST_FILE.dbinded 2> /dev/null
 
 	for _sz in $SIZES ; do
 		pkill -9 ltproto
@@ -19,8 +20,14 @@ if [ F"$1" != F"-g" ] ; then
 
 	for _sz in $SIZES ; do
 		pkill -9 ltproto
-		_data=$($LT_TEST -c -q -b "$_sz"K | tr '\n' '\t')
+		_data=$($LT_TEST -c same -q -b "$_sz"K | tr '\n' '\t')
 		echo "$_sz	$_data" >> $TEST_FILE.binded
+	done
+
+	for _sz in $SIZES ; do
+		pkill -9 ltproto
+		_data=$($LT_TEST -c different -q -b "$_sz"K | tr '\n' '\t')
+		echo "$_sz	$_data" >> $TEST_FILE.dbinded
 	done
 fi
 
@@ -40,10 +47,15 @@ plot	"$TEST_FILE" using 1:(rate(\$2)) title 'TCP' w linespoints, \
 	"$TEST_FILE" using 1:(rate(\$3)) title 'Unix sockets' w linespoints, \
 	"$TEST_FILE" using 1:(rate(\$4)) title 'UDP-shmem' w linespoints, \
 	"$TEST_FILE" using 1:(rate(\$5)) title 'Unix-shmem' w linespoints
-set title "LTproto test binded to CPU"
+set title "LTproto test binded to a same CPU core"
 plot	"$TEST_FILE.binded" using 1:(rate(\$2)) title 'TCP' w linespoints, \
 	"$TEST_FILE.binded" using 1:(rate(\$3)) title 'Unix sockets' w linespoints, \
 	"$TEST_FILE.binded" using 1:(rate(\$4)) title 'UDP-shmem' w linespoints, \
 	"$TEST_FILE.binded" using 1:(rate(\$5)) title 'Unix-shmem' w linespoints
+set title "LTproto test binded to different CPU cores"
+plot	"$TEST_FILE.dbinded" using 1:(rate(\$2)) title 'TCP' w linespoints, \
+	"$TEST_FILE.dbinded" using 1:(rate(\$3)) title 'Unix sockets' w linespoints, \
+	"$TEST_FILE.dbinded" using 1:(rate(\$4)) title 'UDP-shmem' w linespoints, \
+	"$TEST_FILE.dbinded" using 1:(rate(\$5)) title 'Unix-shmem' w linespoints
 EOF
 
