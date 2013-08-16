15041ca5fee63f74de709b0070cb172b2285fe5b
Vsevolod Stakhov <vsevolod@highsecure.ru>
2013-08-16 14:20:16 +0100

    Add shmem ring tests.

---
+++ test/plot_shmem.py
@@ -0,0 +1,51 @@
+import sys, os
+
+import numpy
+import matplotlib
+import csv
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+# ---------------------------
+# Handle command line args
+
+if len(sys.argv) < 3:
+  print "usage: python plot_lat.py <file> <title> [fix-scale] [colourmap]"
+  sys.exit(0)
+
+input_file = sys.argv[1]
+
+fix_scale = 0
+if len(sys.argv) > 3:
+  fix_scale = int(sys.argv[3])
+
+if len(sys.argv) > 4:
+  colourmap = sys.argv[4]
+else:
+  colourmap = "GnBu"
+
+data = map(lambda(x): (8589934592. * 8 / x) / (1024 * 1024) * (1000000000.), numpy.loadtxt(input_file))
+
+fig = plt.figure(figsize=(8,6))
+
+if fix_scale != 0:
+  plt.matshow(data, vmax=0.000006, vmin=0.000001, fignum=0, cmap=colourmap)
+else:
+  plt.matshow(data, fignum=0, cmap=colourmap, origin='lower')
+
+
+plt.ylabel('Ring slots')
+plt.yticks(range(6), (32, 64, 128, 256, 512, 1024), rotation=17)
+#plt.ylim(0, 48)
+plt.xlabel('Ring buffers')
+#plt.xlim(0, 48)
+plt.xticks(range(5), (1024, 4096, 8192, 16384, 32768), rotation=17)
+plt.title(sys.argv[2])
+
+cb = plt.colorbar(shrink=1.0, format='%.3f')
+cb.set_label('Throughput in megabits')
+
+plt.savefig(sys.argv[1] + ".png", format="png", bbox_inches='tight')
+
---
+++ test/test_shmem.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+BSIZES="128 256 512 1024 8192 32768 65536 524288"
+RING_SLOTS="32 64 128 256 512 1024"
+RING_BUFFERS="1024 4096 8192 16384 32768"
+
+for _bsize in $BSIZES ; do
+        _rfile="/tmp/lt_shmem-$_bsize.dat"
+        if [ F"$1" != F"-g" ] ; then
+                rm -f $_rfile
+                for _rslots in $RING_SLOTS ; do
+                        _seq=""
+                        for _rbuf in $RING_BUFFERS ; do
+                                _seq="$_seq `LTPROTO_RING_SLOTS=$_rslots LTPROTO_RING_BUF=$_rbuf test/ltproto_test -t shmem -q -b $_bsize`"
+                        done
+                        echo $_seq >> $_rfile
+                done
+        fi
+        python test/plot_shmem.py $_rfile "Shmem for $_bsize bytes blocks"
+done
